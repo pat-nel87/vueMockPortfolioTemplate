509e7d67702088ba0e07cb81065a6002013f4ce3
pat-nel87 <>
2021-04-19 20:03:45 -0400

    correction

--- components/blogpost.js
+++ components/blogpost.js
@@ -1,13 +1,15 @@
 app.component('fblogpost', {
+  props: {
 
+  },
   template:
   /*html*/
   `<div class="row medium-8 large-7 columns">
     <div class="blog-post">
-      <h3>Awesome blog post title <small>3/6/2015</small></h3>
-      <img class="thumbnail" src="https://placehold.it/850x350">
+      <h3>Awesome blog post title <small> date </small></h3>
+      <img class="thumbnail" v-bind:src="image">
       <p>Praesent id metus massa, ut blandit odio. Proin quis tortor orci. Etiam at risus et justo dignissim congue. Donec congue lacinia dui, a porttitor lectus condimentum laoreet. Nunc eu ullamcorper orci. Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque faucibus vestibulum. Nulla at nulla justo, eget luctus.</p>
-      <div class="callout">
+      <!--<div class="callout">
       <!--  <ul class="menu simple">
           <li><a href="#">Author: Mike Mikers</a></li>
           <li><a href="#">Comments: 3</a></li>
@@ -15,6 +17,28 @@ app.component('fblogpost', {
       </div>
     </div>
 `,
+  data() {
+   return {
+
+     selection: 1,
+     variants: [
+       { id: 1, image: "https://upload.wikimedia.org/wikipedia/commons/d/d1/ISS046-E-29249_-_View_of_Earth.jpg"},
+       { id: 2, image: "https://upload.wikimedia.org/wikipedia/commons/e/ee/Caswell_Cottage_%283947033285%29.jpg"},
+     ]
+   }
+ },
+ methods: {
+
+   setImage(){
+     this.image = "https://upload.wikimedia.org/wikipedia/commons/d/d1/ISS046-E-29249_-_View_of_Earth.jpg"
+   }
+
+ },
+ computed: {
+   image() {
+     return this.variants[this.selection].image
+   },
+ }
 
 
 })
--- components/fnav.js
+++ components/fnav.js
@@ -2,9 +2,9 @@ app.component('f-nav',
 {
   template:
   /*html*/
-  `  <!-- Start Top Bar -->
+  `<!--  <!-- Start Top Bar -->
     <!-- <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css"> -->
-    <div class="top-bar">
+    <!-- <div class="top-bar">
       <div class="top-bar-left">
         <ul class="menu">
           <li class="menu-text">Blog</li>
@@ -13,6 +13,16 @@ app.component('f-nav',
           <li><a href="#">Three</a></li>
         </ul>
       </div>
-    </div>
-<!-- End Top Bar -->`,
+    </div> -->
+<!-- End Top Bar -->
+
+<div class="title-bar">
+  <div class="title-bar-left">
+    <button class="menu-icon" class="dropdown-menu" type="button"></button>
+    <span class="title-bar-title">Patrick's dev portfolio</span>
+  </div>
+  <div class="title-bar-right">
+    <button class="menu-icon" type="button"></button>
+  </div>
+</div>`,
 })
